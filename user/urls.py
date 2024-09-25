21c5e6775450e7376cd72fdfc39de62c93cd7d14
Nika Gabeskiria <nikoloz.gabeskiria@gmail.com>
2024-09-25 21:59:11 +0400

    final updates to project

--- user/urls.py
+++ user/urls.py
@@ -6,9 +6,11 @@ from . import views
 
 
 urlpatterns = [
-    path('create_user/', views.create_user, name='Create_User'),
-    path('user/', views.show_user, name='ShowUser'),
-    path('delete-user/<int:user_id>/', views.delete_user, name='delete_user')
+    path('', views.main_page, name='main_page'),
+    path('create/', views.create_user, name='create_user'),
+    path('delete/', views.delete_user, name='delete_user'),
+    path('show/', views.show_user, name='show_user'),
+    path('next_step/', views.next_step, name='next_step')
 
 
 ]
--- user/views.py
+++ user/views.py
@@ -5,29 +5,45 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 
 
+
+
+def main_page(request):
+    return render(request, 'user/main_page.html')
+
+
 def create_user(request):
     if request.method == "POST":
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('ShowUser')  # Redirect to a success page or another view
+            return redirect('next_step')  # Redirect to a success page or another view
     else:
         form = UserForm()
 
-    return render(request, 'user/user_template.html', {'form': form})
+    return render(request, 'user/create_user.html', {'form': form})
+
+
+
+def next_step(request):
+    return render(request, 'user/next_step.html')
+
+
+
 
 def show_user(request):
-    show_user = User.objects.all()
-
-    return HttpResponse(show_user)
-
+    users = User.objects.all()  # Fetch all users
+    return render(request, 'user/show_user.html', {'users': users})
 
 
-def delete_user(request, user_id):
-    user = get_object_or_404(User, id=user_id)
-    
-    if request.method == "POST":
+
+def delete_user(request):
+    users = User.objects.all()  # Fetch all users
+    if request.method == 'POST':
+        user_id = request.POST.get('user_id')
+        user = get_object_or_404(User, id=user_id)
         user.delete()
-        return redirect('ShowUser')  # Redirect to the view that shows all users after deletion
+        return redirect('delete_user')  # Refresh the page after deletion
 
-    return render(request, 'user/confirm_delete.html', {'user': user})
+    return render(request, 'user/delete_user.html', {'users': users})
+def main_page(request):
+    return render(request, 'user/main_page.html')
